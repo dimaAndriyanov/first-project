0fe2cbaa7196db9703e8f240c9abf03b82ec13b7
Dmitriy <d.79052201159@yandex.ru>
2022-11-10 23:20:18 +0300

    Модифицирован класс Praktikum

--- Praktikum.java
+++ Praktikum.java
@@ -1,5 +1,6 @@
 public class Praktikum {
     public static void main(String[] args) {
         System.out.println("Тут будет тело класса");
+        Converter converter = new Converter();
     }
 }
